8f8fb8a6788f4dd7685a71aa3916b4b95b668068
Julian Grunauer <julian.grunauer@appian.com>
2023-04-04 22:43:28 -0400

    basic one-shot sail generation with streaming enabled kind of working

--- chatgpt/src/Components/chat/Input.jsx
+++ chatgpt/src/Components/chat/Input.jsx
@@ -1,14 +1,52 @@
 import { MDBIcon } from 'mdb-react-ui-kit'
-import React, { useContext, useEffect } from 'react'
+import React, { useCallback, useContext, useEffect } from 'react'
 import { useState } from 'react';
 
 import { USER, GPT } from '../../constants';
 import AppianContext from "../../context/AppianContext"
+import { useEventStream } from '../../hooks/useEventStream';
+import { createSecondPrompt } from '../../Util'
 
 const Input = ({ conversation, setConversation, model, temperature, top_p, n, stop, max_tokens, presence_penalty, frequency_penalty, user, sendButtonColor, setIsLoading }) => {
 	const { Appian, allparameters } = useContext(AppianContext)
 	const [message, setMessage] = useState("")
+	const [currentPrompt, setCurrentPrompt] = useState("");
 	const [connectedSystem, setConnectedSystem] = useState("")
+
+
+
+	const [receivedText, setReceivedText] = useState('');
+	const [startStream, setStartStream] = useState(false);
+
+
+	const handleStreamData = useCallback((data) => {
+		console.log(data);
+		setReceivedText((prevText) => prevText + data + ' ');
+
+		setConversation(prevConvo => {
+			const firstMessageInStream = prevConvo[prevConvo.length - 1]?.role === USER
+			const modifiedConvo = firstMessageInStream ?
+				prevConvo :
+				prevConvo.slice(0, -1)
+			const content = firstMessageInStream ?
+				data :
+				prevConvo[prevConvo.length - 1]?.content + data
+			return [...modifiedConvo, { role: GPT, content }]
+		})
+
+	}, [setConversation]);
+
+	const handleStreamEnd = useCallback((data) => {
+		setStartStream(false)
+	}, []);
+
+	const { streamData, error, loading } = useEventStream(
+		'https://api.openai.com/v1/chat/completions',
+		handleStreamData,
+		startStream,
+		handleStreamEnd,
+		currentPrompt
+	);
 
 	useEffect(() => {
 		// Checking if required connectedSystem parameter exists
@@ -18,6 +56,8 @@ const Input = ({ conversation, setConversation, model, temperature, top_p, n, st
 
 	}, [Appian.Component, allparameters]);
 
+
+
 	async function addItem(e) {
 		e.preventDefault();
 
@@ -26,63 +66,26 @@ const Input = ({ conversation, setConversation, model, temperature, top_p, n, st
 			const updatedConversation = [...conversation, { role: USER, content: message }]
 			setConversation(updatedConversation)
 			setIsLoading(true)
-			setMessage("")
+
 
 			// Setting textarea back to one line
 			const textarea = document.querySelector('.form-control-lg');
 			textarea.style.height = 'initial';
 
-			const response = await handleClientApi(
-				connectedSystem,
-				"chatCompletion",
-				{
-					messages: updatedConversation.filter((item) => !item.hasOwnProperty("error")),
-					model,
-					// if the value is truthy, spread the object into the main payload, if not, don't add it as a param to the payload
-					...(temperature && { temperature }),
-					...(top_p && { top_p }),
-					...(n && { n }),
-					...(stop && { stop }),
-					...(max_tokens && { max_tokens }),
-					...(presence_penalty && { presence_penalty }),
-					...(frequency_penalty && { frequency_penalty }),
-					...(user && { user })
-				}
-			)
+
+			//TODO: openai call
+			setStartStream((prevStartStream) => !prevStartStream);
+			setCurrentPrompt(message)
+			setMessage("")
+
 
 			setIsLoading(false)
 
-			// Response
-			response?.payload?.error ?
-				setConversation(
-					[
-						...updatedConversation,
-						{
-							role: GPT,
-							content: `Unable to connect to ChatGPT. Error: ${JSON.stringify(JSON.stringify(response?.payload?.error?.message)).replace(/"/g, '\\"').replace(/\\|"|'/g, '')}, ErrorCode: ${JSON.stringify(JSON.stringify(response?.payload?.error?.type)).replace(/"/g, '\\"').replace(/\\|"|'/g, '')}`, error: true
-						}
-					]) :
-				setConversation(
-					[
-						...updatedConversation,
-						...response?.payload?.messages.map(curr => {
-							return {
-								role: GPT,
-								content: new TextDecoder("utf-8").decode(new Uint8Array([...atob(curr)].map(char => char.charCodeAt(0))))
-							}
-						})
-					]
-				)
+
 		}
 	}
 
-	async function handleClientApi(connectedSystem, friendlyName, payload) {
-		return await Appian.Component.invokeClientApi(
-			connectedSystem,
-			friendlyName,
-			payload
-		);
-	}
+
 
 	return (
 		<div className="text-muted d-flex justify-content-start align-items-start ">
--- chatgpt/src/Util.js
+++ chatgpt/src/Util.js
@@ -10,3 +10,92 @@ export function isValidUrl(str) {
 		return false;
 	}
 }
+
+
+const THREE_COLUMN_FORM = `a!localVariables(
+	local!cancel: false,
+	a!formLayout(
+		label: "Form",
+		contents: {
+		  a!sectionLayout(
+			contents: {}
+		  ),
+		  a!sectionLayout(
+			label: "Section",
+			contents: {
+			  a!columnsLayout(
+				columns: {
+				  a!columnLayout(
+					contents: {}
+				  ),
+				  a!columnLayout(
+					contents: {}
+				  ),
+				  a!columnLayout(
+					contents: {}
+				  )
+				}
+			  )
+			}
+		  )
+		},
+		buttons: a!buttonLayout(
+		  primaryButtons: {
+			a!buttonWidget(
+			  label: "Submit",
+			  submit: true,
+			  style: "PRIMARY"
+			)
+		  },
+		  secondaryButtons: {
+			a!buttonWidget(
+			  label: "Cancel",
+			  value: true,
+			  saveInto: local!cancel,
+			  submit: true,
+			  style: "NORMAL",
+			  validate: false
+			)
+		  }
+		)
+	  )
+)`
+
+
+export function createSAIL() {
+
+	return `You are an Appian SAIL interface generator. Modify the following interface based on the prompt.
+
+	INTERFACE TO MODIFY: ${THREE_COLUMN_FORM}
+	
+	RULES:
+	- Do not make up any new functions. 
+	- Do not use parameters outside of those listed in the functions context below.
+	- Try to use as many parameters as possible
+	- Always set parameters of functions. For example, a!dateField(label: "Date", value: now(), saveInto: now(), readOnly: true) is correct while a!dateField(label: "Date", value: now(), saveInto: now(), readOnly: ) is incorrect because the value of readOnly was not set
+	- Send back only SAIL code.
+	- Start all of your SAIL responses with a!localVariables
+	- If using a!dropDownField, a!radioButtonField, or any other fields with the paramters choiceLabels and choiceValues, always make sure to set both choiceLabels and choiceValues. choiceLabels and choiceValues should have the same value
+	- Only use parameters that are in each function's description, do not make up your own. For example, a!formLayout has the parameters label, instructions, contents, buttons, validations, validationGroup, skipAutoFocus, showWhen shown in a!formLayout(label, instructions, contents, buttons, validations, validationGroup, skipAutoFocus, showWhen). Do not give a!formLayout a 'placeholder' because it is not in the list of it's parameters.
+	- Always set the value of fields
+	- Only reference local variables that are set at the beginning of the interface within a!localVariables. For example, a!localVariables(local!name, a!textField(label: "Name", value: local!name, saveInto: local!name, required: true)) is correct while a!localVariables(local!date, a!textField(label: "Name", value: local!name, saveInto: local!name, required: true)) is incorrect because the local!name variable is not set at the beginning of a!localVariables
+	- Only use local variables that are declared at the top of the interface and set them with a default value related to their use. Declare all local variables at the top of the interface with:
+		a!localVariables(
+			local!textVar: "",
+			local!integerVar: 0,
+				<INSERT OTHER SAIL FUNCTIONS HERE>
+			)
+
+	ERRORS TO AVOID:
+	- An array of components cannot contain a form layout, dashboard layout, or column layout. For example, a!localVariables(local!example, formLayout(<INSERT OTHER SAIL FUNCTIONS HERE>)) is correct and  a!localVariables(local!example, { formLayout(<INSERT OTHER SAIL FUNCTIONS HERE>) }) is not correct
+	- Always set the placeholder parameter of a!dropdownField components
+	- Always have matching brackets and parenthesis
+	- Logical operators like or(), when(), and and() must wrap around the condition. For example and(local!val, local!val2) or or(local!val, local!val2) or when({true, false, true})
+	- Always wrap the value of the parameter contents in brackets. For example: a!formLayout(label: "Home Loan Form", contents: {a!textField(), a!textField} )
+	- The contents field on a column layout or a formLayout cannot contain a ButtonWidget.
+	- Do not make up functions. If you feel like you need to use a function that does not exist, simply comment it out. For example, isemail() is not a SAIL function, so if you want to use it, comment it out like this: /* validations: isemail("email") */
+	- The contents field on a column layout cannot contain a ButtonWidget. 
+	- A header content layout has an invalid value for "header". Header must be null, a billboard, a card, or a list of billboards or cards.
+	
+	`
+}
